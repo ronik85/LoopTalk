4d3da971e94b286a6cadf947b5849e2d8b84162c
Ronik <ronik.kalanouria@gmail.com>
2025-01-22 17:49:10 +0530

    Protected Route and Not found page added

--- loopTalk-fe/src/App.jsx
+++ loopTalk-fe/src/App.jsx
@@ -1,22 +1,37 @@
-import React, { lazy } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/styles/ProtectedRoute";
 
-const Home = lazy(() => import('./pages/Home'))
-const Login = lazy(() => import('./pages/Login'))
-const Chat = lazy(() => import('./pages/Chat'))
-const Groups = lazy(() => import('./pages/Groups'))
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
+let user = false;
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/chat/:chatid" element={<Chat />} />
-        <Route path="/groups" element={<Groups />} />
+        <Route element={<ProtectedRoute user={user} />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat/:chatid" element={<Chat />} />
+          <Route path="/groups" element={<Groups />} />
+        </Route>
+
+        <Route
+          path="/login"
+          element={
+            <ProtectedRoute user={!user} redirect="/">
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ loopTalk-fe/src/components/styles/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const ProtectedRoute = ({ children, user, redirect = '/login' }) => {
+    if (!user)
+        return <Navigate to={redirect} />
+    else return children ? children : <Outlet />
+}
+
+export default ProtectedRoute
