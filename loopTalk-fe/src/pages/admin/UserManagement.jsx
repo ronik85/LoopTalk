74fa8798da1b25af3fe2179ee353dc7849c57cd1
Ronik <ronik.kalanouria@gmail.com>
2025-01-28 19:22:48 +0530

    Admin dashboard ui completed

--- loopTalk-fe/src/pages/admin/UserManagement.jsx
+++ loopTalk-fe/src/pages/admin/UserManagement.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminLayout from '../../components/layout/AdminLayout'
+import Table from '../../components/shared/Table';
+import { transformImage } from "../../lib/features";
+import { Avatar, Skeleton } from "@mui/material";
+import { dashboardData } from '../../constants/SampleData'
+const columns = [
+    {
+        field: "id",
+        headerName: "ID",
+        headerClassName: "table-header",
+        width: 200,
+    },
+    {
+        field: "avatar",
+        headerName: "Avatar",
+        headerClassName: "table-header",
+        width: 150,
+        renderCell: (params) => (
+            <Avatar alt={params.row.name} src={params.row.avatar} />
+        ),
+    },
+
+    {
+        field: "name",
+        headerName: "Name",
+        headerClassName: "table-header",
+        width: 200,
+    },
+    {
+        field: "username",
+        headerName: "Username",
+        headerClassName: "table-header",
+        width: 200,
+    },
+    {
+        field: "friends",
+        headerName: "Friends",
+        headerClassName: "table-header",
+        width: 150,
+    },
+    {
+        field: "groups",
+        headerName: "Groups",
+        headerClassName: "table-header",
+        width: 200,
+    },
+];
+
+
 
 const UserManagement = () => {
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => {
+        setRows(dashboardData.users.map((i) => ({ ...i, id: i._id, avatar: transformImage(i.avatar) })))
+    }, [])
     return (
         <AdminLayout>
-            <>
-                UserManagement
-            </>
+            <Table heading={"All Users"} columns={columns} rows={rows} />
         </AdminLayout>
     )
 }
